248ff2f80ca509cd65c71d1e498f490961e9291c
Unanoc <leedaniil98@gmail.com>
2018-11-25 20:35:42 +0300

    checking on timestamp type

--- posgresExporter/database/query.go
+++ posgresExporter/database/query.go
@@ -2,28 +2,46 @@ package database
 
 import (
 	"fmt"
+	"log"
+	"reflect"
+	"time"
 
 	"github.com/jackc/pgx"
 )
 
-func Query(conn *pgx.ConnPool, queryString string) error {
+// Query selects data from table and then sends record by record in chanel.
+func Query(conn *pgx.ConnPool, queryString string, recordChan chan<- []string) {
 
 	rows, err := conn.Query(queryString)
 	defer rows.Close()
 
 	if err != nil {
-		return err
+		log.Println(err)
 	}
 
 	fields := rows.FieldDescriptions()
 
+	titles := make([]string, 1)
 	for _, value := range fields {
-		fmt.Println(value.Name, value.DataTypeName)
+		titles = append(titles, value.Name)
 	}
+
+	recordChan <- titles
 
 	for rows.Next() {
-		fmt.Println(rows.Values())
+		rowsRecord := make([]string, 1)
+		values, _ := rows.Values()
+		for _, value := range values {
+			if reflect.TypeOf(value).String() == "time.Time" {
+				time := value.(time.Time)
+				rowsRecord = append(rowsRecord, fmt.Sprintf("%d", time.Unix()))
+			} else {
+				row := fmt.Sprintln(value)
+				rowsRecord = append(rowsRecord, row[:len(row)-1]) // remove "\n"
+			}
+		}
+		recordChan <- rowsRecord
 	}
 
-	return nil
+	// return nil
 }
