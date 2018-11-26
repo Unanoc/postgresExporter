3cbd4032fa696deee3b48c7ace952ea557358d3a
Unanoc <leedaniil98@gmail.com>
2018-11-26 17:01:43 +0300

    added checking channel

--- posgresExporter/api/export.go
+++ posgresExporter/api/export.go
@@ -17,10 +17,14 @@ func WorkerExport(ctx context.Context, wg *sync.WaitGroup, conn *pgx.ConnPool, o
 		select {
 		case <-ctx.Done():
 			return
-		case task := <-tasks:
-			recordChan := make(chan []string)
-			go CreateCSV(ctx, task.MaxLines, outputDir, task.Name, recordChan)
-			database.Query(conn, task.Query, task.Name, recordChan)
+		case task, ok := <-tasks:
+			if ok {
+				recordChan := make(chan []string)
+				go CreateCSV(ctx, task.MaxLines, outputDir, task.Name, recordChan)
+				database.Query(conn, task.Query, task.Name, recordChan)
+			} else {
+				return
+			}
 		}
 	}
 }
