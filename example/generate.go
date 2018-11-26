a361efbf19baa51a3a4bd7d2296536ced0f0d79d
Unanoc <leedaniil98@gmail.com>
2018-11-26 16:40:22 +0300

    generating of 5 tables

--- example/generate.go
+++ example/generate.go
@@ -24,8 +24,8 @@ func main() {
 	}
 	defer conn.Close()
 
-	fmt.Println("-- TABLE PEOPLE --")
-	for i := 0; i < 10; i++ {
+	fmt.Println("-- TABLE USERS --")
+	for i := 0; i < 1000; i++ {
 		firstname := fake.FirstName()
 		lastname := fake.LastName()
 		email := fake.EmailAddress()
@@ -35,30 +35,84 @@ func main() {
 
 		_, err = conn.Exec(`
 			INSERT
-			INTO people ("firstname", "lastname", "email", "phone", "about", "birthday")
+			INTO users ("firstname", "lastname", "email", "phone", "about", "birthday")
 			VALUES ($1, $2, $3, $4, $5, $6)`,
 			&firstname, &lastname, &email, &phone, &text, &time)
 
 		if err != nil {
 			log.Print(err)
 		}
-		fmt.Printf("#%d generated...\n", i)
 	}
+	fmt.Println("Table 'users' is generated...")
 
-	fmt.Println("-- TABLE CITIES --")
-	for i := 0; i < 10; i++ {
+	fmt.Println("-- TABLE VILAGES --")
+	for i := 0; i < 1000; i++ {
 		cityName := fake.City()
 		population := rand.Int31()
 
 		_, err = conn.Exec(`
 			INSERT
-			INTO cities ("name", "population")
+			INTO vilages ("name", "population")
 			VALUES ($1, $2)`,
 			&cityName, &population)
 
 		if err != nil {
 			log.Print(err)
 		}
-		fmt.Printf("#%d generated...\n", i)
 	}
+	fmt.Println("Table 'vilages' is generated...")
+
+	fmt.Println("-- TABLE PEOPLE --")
+	for i := 0; i < 1000; i++ {
+		firstname := fake.FirstName()
+		lastname := fake.LastName()
+		flag := rand.Int31()
+		time := time.Now()
+
+		_, err = conn.Exec(`
+			INSERT
+			INTO people ("name", "lastname", "birthday", "some_flag", "created")
+			VALUES ($1, $2, current_timestamp, $3, $4)`,
+			&firstname, &lastname, &flag, &time)
+
+		if err != nil {
+			log.Print(err)
+		}
+	}
+	fmt.Println("Table 'people' is generated...")
+
+	fmt.Println("-- TABLE CITIES --")
+	for i := 0; i < 1000; i++ {
+		cityName := fake.City()
+		countryID := rand.Int31()
+		time := time.Now()
+
+		_, err = conn.Exec(`
+			INSERT
+			INTO cities ("name", "country_id", "created")
+			VALUES ($1, $2, $3)`,
+			&cityName, &countryID, &time)
+
+		if err != nil {
+			log.Print(err)
+		}
+	}
+	fmt.Println("Table 'cities' is generated...")
+
+	fmt.Println("-- TABLE COUNTRIES --")
+	for i := 0; i < 1000; i++ {
+		country := fake.Country()
+		time := time.Now()
+
+		_, err = conn.Exec(`
+			INSERT
+			INTO countries ("name", "created")
+			VALUES ($1, $2)`,
+			&country, &time)
+
+		if err != nil {
+			log.Print(err)
+		}
+	}
+	fmt.Println("Table 'countries' is generated...")
 }
--- example/schema.sql
+++ example/schema.sql
@@ -1,9 +1,12 @@
+DROP TABLE IF EXISTS "users" CASCADE;
+DROP TABLE IF EXISTS "vilages" CASCADE;
 DROP TABLE IF EXISTS "people" CASCADE;
 DROP TABLE IF EXISTS "cities" CASCADE;
-DROP TABLE IF EXISTS "psqltypes" CASCADE;
+DROP TABLE IF EXISTS "countries" CASCADE;
 
--- TABLE "people" --
-CREATE TABLE IF NOT EXISTS people (
+
+-- TABLE "users" --
+CREATE TABLE IF NOT EXISTS users (
   "email"     CITEXT UNIQUE NOT NULL PRIMARY KEY,
   "firstname" CITEXT NOT NULL,
   "lastname"  CITEXT NOT NULL,
@@ -12,47 +15,33 @@ CREATE TABLE IF NOT EXISTS people (
   "birthday"  timestamp not null
 );
 
--- TABLE "cities" --
-CREATE TABLE IF NOT EXISTS cities (
+-- TABLE "vilages" --
+CREATE TABLE IF NOT EXISTS vilages (
   "name"       CITEXT NOT NULL,
   "population" INTEGER DEFAULT 0
 );
 
--- TABLE "psqltypes" --
-CREATE TABLE IF NOT EXISTS psqltypes (
-  "bigint"        int8 NOT NULL,
-  "bigserial"     serial8 NOT NULL,
-  "bit"           bit[5] NOT NULL,
-  "varbit"        varbit NOT NULL,
-  "boolean"       boolean NOT NULL,
-  "box"           box NOT NULL,
-  "character"     char[5] NOT NULL,
-  "cidr"          cidr NOT NULL,
-  "circle"        circle NOT NULL,
-  "date"          date NOT NULL,
-  "float8"        float8 NOT NULL,
-  "inet"          inet NOT NULL,
-  "integer"       int NOT NULL,
-  "interval"      interval[?] NOT NULL,
-  "json"          json NOT NULL,
-  "line"          line NOT NULL,
-  "lseg"          lseg NOT NULL,
-  "macaddr"       macaddr NOT NULL,
-  "money"         int8 NOT NULL,
-  "numeric"       decimal[?] NOT NULL,
-  "path"          path NOT NULL,
-  "point"         point NOT NULL,
-  "polygon"       polygon NOT NULL,
-  "real"          float4 NOT NULL,
-  "smallint"      int2 NOT NULL,
-  "smallserial"   serial2 NOT NULL,
-  "serial"        serial4 NOT NULL,
-  "text"          text NOT NULL,
-  "time"          time [?][?] NOT NULL,
-  "timestamp"     timestamp[?] [?] NOT NULL,
-  "tsquery"       tsquery NOT NULL,
-  "tsvector"      tsvector NOT NULL,
-  "txid_snapshot" txid_snapshot NOT NULL,
-  "uuid"          uuid NOT NULL,
-  "xml"           xml NOT NULL
+-- TABLE "people" --
+create table people (
+  "id"        serial not null primary key,
+  "name"      varchar(256) not null,
+  "lastname"  varchar(256) not null,
+  "birthday"  date not null,
+  "some_flag" integer not null,
+  "created"   timestamp(0) not null default current_timestamp
+);
+
+-- TABLE "cities" --
+create table cities (
+  "id"         serial not null primary key,
+  "name"       varchar(256) not null,
+  "country_id" integer not null,
+  "created"    timestamp(0) not null default current_timestamp
+);
+
+-- TABLE "countries" --
+create table countries (
+  "id"      serial not null primary key,
+  "name"    varchar(256) not null,
+  "created" timestamp(0) not null default current_timestamp
 );
